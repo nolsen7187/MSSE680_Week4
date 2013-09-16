fe5739594c8f2dfb2151f46cea4501e14c1a7213
nolsen <nolsen@regis.edu>
2013-09-16 07:32:59 -0600

    Version 1 of Week 4, Factory Class Added

---
+++ FFR/FFR/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFR
+{
+    public class Class1
+    {
+    }
+    namespace Domain
+    {
+
+    }
+    namespace Business
+    {
+
+    }
+    namespace Presentation
+    {
+
+    }
+    namespace Service
+    {
+
+    }
+}
